859e4e3082c5635b35121263d1d6daa508d3dbc2
Hadeel Abdellatif <hadeel.abdellatif2001@gmail.com>
2024-05-14 14:09:58 +0300

    first commit

---
+++ DataAnnotationTask/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DataAnnotationTask.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAnnotationTask.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.; database=dataAnnotationTask; trusted_connection=true; TrustServerCertificate=true");
+
+        }
+    }
+}
